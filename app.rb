396cb293e0ff2f60713562e94faa817b5be1ae07
Mike West <westmd23@gmail.com>
2016-03-29 15:48:29 -0400

    Test working for looking up employee list and single employee

--- app.rb
+++ app.rb
@@ -1,7 +1,21 @@
 require 'rubygems'
 require 'bundler/setup'
 require 'sinatra'
+require 'json'
+require './migrations'
+require './employee'
+require './development'
 
-get '/' do
-  "I am Groot"
+
+before do
+  content_type 'application/json'
+end
+
+get '/employee' do
+  Employee.all.to_json
+end
+
+get '/employee/:id' do
+  employee = Employee.find(params["id"])
+  employee.to_json
 end
--- tests/app_test.rb
+++ tests/app_test.rb
@@ -7,15 +7,27 @@ class AppTest < Minitest::Unit::TestCase
     Sinatra::Application
   end
 
-  def test_it_says_hello_world
-    get '/'
-    assert last_response.ok?
-    assert_equal 'I am Groot', last_response.body
+  def test_students_returns_a_list_of_students
+    get '/employee'
+    json_response = JSON.parse(last_response.body)
+    assert_equal true, json_response.first.has_key?("id")
   end
 
-  def test_it_says_hello_to_a_person
-    get '/', :name => 'Simon'
-    assert last_response.body.include?('Groot')
-    assert_equal 'I am Groot', last_response.body
+  def test_can_read_single_employee
+    get '/employee/8'
+    employee = JSON.parse(last_response.body)
+    assert_equal "Elanor", employee["name"]
   end
+  # def test_it_says_hello_world
+  #   get '/'
+  #   assert last_response.ok?
+  #   assert_equal 'I am Groot', last_response.body
+  # end
+  #
+  # def test_it_says_hello_to_a_person
+  #   get '/', :name => 'Simon'
+  #   assert last_response.body.include?('Groot')
+  #   assert_equal 'I am Groot', last_response.body
+  # end
+
 end
